b94b33acc1eadc375aafba876eb29f9071113fd6
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-23 23:37:39 +0900

    GearからWheelを作成する処理を除去

--- chapter09/chapter03.rb
+++ chapter09/chapter03.rb
@@ -12,17 +12,16 @@ class Wheel
 end
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
-    @rim = args[:rim]
-    @tire = args[:tire]
+    @wheel = args[:wheel]
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 
   def ratio
