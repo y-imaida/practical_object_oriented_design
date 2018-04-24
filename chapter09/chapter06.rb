81219682de138cd890e4eb871974df2c7c7360c3
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 14:43:34 +0900

    第6章のサンプルコードを追加

---
+++ chapter09/chapter06.rb
@@ -0,0 +1,46 @@
+class Bicycle
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(args = {})
+    @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  def post_initialize(args)
+    nil
+  end
+
+  def spares
+    { tire_size: tire_size, chain: chain }.merge(local_spares)
+  end
+
+  def local_spares
+    {}
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def default_tire_size
+    '23'
+  end
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    { tape_color: tape_color }
+  end
+end
