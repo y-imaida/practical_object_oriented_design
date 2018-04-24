96a1352dff8265243fb1b508c22db5fb0a6ecdb7
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 15:15:35 +0900

    サブクラスに共通するインターフェイスに関するテストを追加

--- chapter09/test/bicycle_test.rb
+++ chapter09/test/bicycle_test.rb
@@ -4,4 +4,8 @@ class BicycleTest < MiniTest::Unit::TestCase
   def setup
     @bike = @object = Bicycle.new({ tire_size: 0 })
   end
+
+  def test_forces_subclasses_to_implement_default_tire_size
+    assert_raises(NotImplementedError) { @bike.default_tire_size }
+  end
 end
--- chapter09/test/road_bike_test.rb
+++ chapter09/test/road_bike_test.rb
@@ -1,5 +1,6 @@
 class RoadBikeTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
+  include BicycleSubclassTest
 
   def setup
     @bike = @object = RoadBike.new
