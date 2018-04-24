4d73a51caca529f549b2769f4bd4c53fe82bc9bc
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 10:50:37 +0900

    observerへの送信メッセージの検証をGearのテストに追加

--- chapter09/test/gear_test.rb
+++ chapter09/test/gear_test.rb
@@ -5,6 +5,15 @@ class DiameterDouble
 end
 
 class GearTest < MiniTest::Unit::TestCase
+  def setup
+    @observer = MiniTest::Mock.new
+    @gear = Gear.new(
+              chainring: 52,
+              cog: 11,
+              observer: @observer
+    )
+  end
+
   def test_calculates_gear_inches
     gear = Gear.new(
             chainring: 52,
@@ -13,5 +22,17 @@ class GearTest < MiniTest::Unit::TestCase
     )
 
     assert_in_delta(47.27, gear.gear_inches, 0.01)
+  end
+
+  def test_notifies_observers_when_cogs_change
+    @observer.expect(:changed, true, [52, 27])
+    @gear.set_cog(27)
+    @observer.verify
+  end
+
+  def test_notifies_observers_when_chainrings_change
+    @observer.expect(:changed, ture, [42, 11])
+    @gear.set_chainring(42)
+    @observer.verify
   end
 end
