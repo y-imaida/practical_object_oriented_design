bed4b36c1c30406bc7cdb377451dda87f1448dba
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 11:34:30 +0900

    ダックタイプへの送信メッセージを検証するテストを追加

---
+++ chapter09/test/trip_test.rb
@@ -0,0 +1,9 @@
+class TripTest < MiniTest::Unit::TestCase
+  def test_requests_trip_preparation
+    @preparer = MiniTest::Mock.new
+    @trip = Trip.new
+    @preparer.expect(:prepare_trip, nil, [@trip])
+    @trip.prepare([@preparer])
+    @preparer.verify
+  end
+end
