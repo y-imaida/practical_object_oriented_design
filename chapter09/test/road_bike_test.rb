0d9e5542f1188b2f5b02987508a6a84162051489
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 15:20:41 +0900

    サブクラス固有のインターフェイスに関するテストを追加

--- chapter09/test/road_bike_test.rb
+++ chapter09/test/road_bike_test.rb
@@ -3,6 +3,10 @@ class RoadBikeTest < MiniTest::Unit::TestCase
   include BicycleSubclassTest
 
   def setup
-    @bike = @object = RoadBike.new
+    @bike = @object = RoadBike.new(tape_color: 'red')
+  end
+
+  def test_puts_tape_color_in_local_spares
+    assert_equal 'red', @bike.local_spares[:tape_color]
   end
 end
