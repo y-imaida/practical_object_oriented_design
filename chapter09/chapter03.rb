fad60c96355e7702e319f5ae9bd6827b4c48e186
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 10:42:25 +0900

    Gearにobserverを追加

--- chapter09/chapter03.rb
+++ chapter09/chapter03.rb
@@ -12,12 +12,13 @@ class Wheel
 end
 
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :wheel, :observer
 
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
     @wheel = args[:wheel]
+    @observer = args[:observer]
   end
 
   def gear_inches
@@ -26,5 +27,19 @@ class Gear
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def set_cog(new_cog)
+    @cog = new_cog
+    changed
+  end
+
+  def set_chainring(new_chainring)
+    @chainring = new_chainring
+    changed
+  end
+
+  def changed
+    observer.changed(chainring, cog)
   end
 end
