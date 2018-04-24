f1f8262fe454e685fdfe6bf85b9226f5cedf5994
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-24 11:01:37 +0900

    第5章のサンプルコードを追加

---
+++ chapter09/chapter05.rb
@@ -0,0 +1,27 @@
+class Mechanic
+  def prepare_trip(trip)
+    trip.bicycles.each { |bicycle| prepare_bicycle(bicycle) }
+  end
+end
+
+class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+end
+
+class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+end
+
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  def prepare(preparers)
+    preparers.each { |preparer| preparer.prepare_trip(self) }
+  end
+end
